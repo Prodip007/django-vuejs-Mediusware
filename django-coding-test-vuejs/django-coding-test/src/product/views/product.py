765beff749d97d9ae969ab71e50c96f940530c1b
Prodip <prodip@tecognize.com>
2023-05-27 22:05:37 +0600

    Listing of the products on the list page is done.

--- django-coding-test-vuejs/django-coding-test/src/product/views/product.py
+++ django-coding-test-vuejs/django-coding-test/src/product/views/product.py
@@ -1,6 +1,8 @@
+from typing import Any, Dict
 from django.views import generic
+from django.views.generic import ListView
 
-from product.models import Variant
+from product.models import Variant, Product, ProductVariant, ProductVariantPrice
 
 
 class CreateProductView(generic.TemplateView):
@@ -11,4 +13,16 @@ class CreateProductView(generic.TemplateView):
         variants = Variant.objects.filter(active=True).values('id', 'title')
         context['product'] = True
         context['variants'] = list(variants.all())
+        return context
+    
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/list.html'
+    paginate_by = 2
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # total_product = Product.objects.all().values('id', 'product_variant_one', 'product_variant_two', 'product_variant_three', 'price', 'stock', 'product')
+        # context["variants"] = list(variants)
         return context
