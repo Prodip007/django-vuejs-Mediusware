b26fd4d6fa6e36fd1ec3a338b03adb0e382ce4fb
Prodip <prodip@tecognize.com>
2023-05-28 18:34:25 +0600

    Date filter is done

--- django-coding-test-vuejs/django-coding-test/src/product/views/product.py
+++ django-coding-test-vuejs/django-coding-test/src/product/views/product.py
@@ -2,6 +2,7 @@ from typing import Any, Dict
 from django.views import generic
 from django.shortcuts import get_object_or_404
 from django.db.models import Q
+import datetime
 # from django.views.generic import ListView
 
 from product.models import Variant, Product, ProductVariant, ProductVariantPrice
@@ -31,14 +32,21 @@ class ProductListView(generic.ListView):
             product_name = self.request.GET.get('title', None)
             product_variant = self.request.GET.get('variant', None)
             price_from = self.request.GET.get('price_from',None)
-            price_to = self.request.GET.get('price_to')
+            price_to = self.request.GET.get('price_to', None)
+            product_date = self.request.GET.get('date', None)
             object_list = []
 
             if product_name:
-                object_list = Product.objects.filter(title=product_name)
-            elif product_variant:
-                object_list = Product.objects.filter(productvariant=product_variant)
-            elif price_from or price_to:
+                s_object_list = Product.objects.filter(title=product_name)
+                for item in s_object_list:
+                    if item not in object_list:
+                        object_list.append(item)
+            if product_variant:
+                s_object_list = Product.objects.filter(productvariant=product_variant)
+                for item in s_object_list:
+                    if item not in object_list:
+                        object_list.append(item)
+            if price_from or price_to:
                 productvariantprice_list = []
                 if price_from:
                     s_productvariantprice_list = ProductVariantPrice.objects.filter(price__gt=price_from)
@@ -52,6 +60,11 @@ class ProductListView(generic.ListView):
                     s_obj = get_object_or_404(Product, productvariantprice=item)
                     if s_obj not in object_list:
                         object_list.append(s_obj)
+            if product_date:
+                s_obj = Product.objects.filter(created_at__date=product_date)
+                for item in s_obj:
+                    if s_obj not in object_list:
+                        object_list.append(item)
             return object_list            
 
     def get_context_data(self, **kwargs: Any):
